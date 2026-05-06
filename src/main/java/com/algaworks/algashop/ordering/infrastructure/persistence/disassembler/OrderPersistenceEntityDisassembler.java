2fec2ad5eb7ae9485a953c4b2b9fc0d6ec3f43f4
Bruno Ramos Lemos <bruno.lemos@live.com>
2026-05-05 22:15:49 -0300

    Feat: updating Assembler and Disassembler for Order and OrderPersistenceEntity

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/disassembler/OrderPersistenceEntityDisassembler.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/disassembler/OrderPersistenceEntityDisassembler.java
@@ -3,10 +3,12 @@ package com.algaworks.algashop.ordering.infrastructure.persistence.disassembler;
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
 import com.algaworks.algashop.ordering.domain.model.entity.OrderStatus;
 import com.algaworks.algashop.ordering.domain.model.entity.PaymentMethod;
-import com.algaworks.algashop.ordering.domain.model.valueobject.Money;
-import com.algaworks.algashop.ordering.domain.model.valueobject.Quantity;
+import com.algaworks.algashop.ordering.domain.model.valueobject.*;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.AddressEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.BillingEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.ShippingEmbeddable;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.stereotype.Component;
 
@@ -29,6 +31,49 @@ public class OrderPersistenceEntityDisassembler {
                 .canceledAt(persistenceEntity.getCanceledAt())
                 .readyAt(persistenceEntity.getReadyAt())
                 .items(new HashSet<>())
+                .billing(toDomainBilling(persistenceEntity.getBilling()))
+                .shipping(toDomainShipping(persistenceEntity.getShipping()))
+                .build();
+    }
+
+    private Shipping toDomainShipping(ShippingEmbeddable shipping) {
+        return Shipping.builder()
+                .cost(new Money(shipping.getCost()))
+                .expectedDate(shipping.getExpectedDate())
+                .recipient(
+                        Recipient.builder()
+                                .fullName(new FullName(shipping.getRecipient().getFirstName(), shipping.getRecipient().getLastName()))
+                                .document(new Document(shipping.getRecipient().getDocument()))
+                                .phone(new Phone(shipping.getRecipient().getPhone()))
+                                .build()
+                )
+                .address(toDomainAddress(shipping.getAddress()))
+                .build();
+    }
+
+    private Billing toDomainBilling(BillingEmbeddable billing) {
+        if (billing == null) return null;
+
+        return Billing.builder()
+                .fullName(new FullName(billing.getFirstName(), billing.getLastName()))
+                .phone(new Phone(billing.getPhone()))
+                .email(new Email(billing.getEmail()))
+                .document(new Document(billing.getDocument()))
+                .address(toDomainAddress(billing.getAddress()))
+                .build();
+    }
+
+    private Address toDomainAddress(AddressEmbeddable address) {
+        if (address == null) return null;
+
+        return Address.builder()
+                .street(address.getStreet())
+                .number(address.getNumber())
+                .city(address.getCity())
+                .state(address.getState())
+                .complement(address.getComplement())
+                .neighborhood(address.getNeighborhood())
+                .zipCode(new ZipCode(address.getZipCode()))
                 .build();
     }
 
